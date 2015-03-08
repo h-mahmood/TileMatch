45e79ce979131f1269e3ffdcb66c2e7041ff93ec
h-mahmood <hmahmood@umich.edu>
2015-03-07 20:08:31 -0500

    Added code that reads in the difficulty setting before launching the game

--- app/src/main/java/com/eecs481/tilematch/MenuScreen.java
+++ app/src/main/java/com/eecs481/tilematch/MenuScreen.java
@@ -7,24 +7,39 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class MenuScreen extends ActionBarActivity {
 
     public void playButtonClick(View v) {
-
         Log.i("[btn]", "playButtonClick in MenuScreen.java");
-        startActivity(new Intent(this, MediumGame.class));
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        String difficultyLevel = settings.getString("select_difficulty", "NULL");
+
+        if (difficultyLevel.equals("1")) {
+            Log.i("[MenuScreen]", "Selected EasyGame");
+            //startActivity(new Intent(this, EasyGame.class));
+        }
+        else if (difficultyLevel.equals("2")) {
+            Log.i("[MenuScreen]", "Selected MediumGame");
+            startActivity(new Intent(this, MediumGame.class));
+        }
+        else if (difficultyLevel.equals("3")) {
+            Log.i("[MenuScreen]", "Selected HardGame");
+            //startActivity(new Intent(this, HardGame.class));
+        }
+        else
+            Log.i("[MenuScreen]", "difficultyLevel not expected: " + difficultyLevel);
     }
 
     public void settingButtonClick(View v) {
-
         Log.i("[btn]", "settingButtonClick in MenuScreen.java");
         startActivity(new Intent(this, GameSettings.class));
     }
 
     public void exitButtonClick(View v) {
-
         Log.i("[btn]", "exitButtonClick in MenuScreen.java");
         this.finish();
     }
